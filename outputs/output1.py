97e1c5a1b4f4717e57c47faf8dce43e47d59f877
MahmoudMohsenEgy <MahmoudMohsenEgy@gmail.com>
2022-05-21 11:50:11 +0200

    changes

--- outputs/output1.py
+++ outputs/output1.py
@@ -1,27 +1,16 @@
-class Dog:
-	def __init__(self,name,breed,age,color):
-		self.name=name
-		self.breed=breed
-		self.age=age
-		self.color=color
+class Person:
+	def __init__(self,Name):
+		self.name=Name
 	def getName(self,):
 		return name
-	def getBreed(self,):
-		return breed
-	def getColor(self,):
-		return color
-	def getAge(self,):
-		return age
-	def setName(self,Name):
-		self.name=Name
-	def setBreed(self,Breed):
-		self.breed=Breed
-	def setColor(self,Color):
-		self.color=Color
-	def setAge(self,Age):
-		self.age=Age
-dog1=Dog("Kovu","German Shepherd",2,"Brown")
-print(dog1.name)
-print(dog1.breed)
-dog1.setName("Kovo")
-print(dog1.name)
+	def isEmployee(self,):
+		return False
+class Employee(Person):
+	def isEmployee(self,):
+		return True
+emp=Person("Ahmed")
+print(emp.getName()
+)
+emp2=Employee("Mohamed")
+print(emp2.getName()
+)
