99222a8d7b1727297b77bb1bc39af4ff20897cff
Yousef <yousefshafik234@yahoo.com>
2022-05-21 10:50:08 +0200

    fixed getter errors

--- outputs/output1.py
+++ outputs/output1.py
@@ -1,27 +1,11 @@
 class Dog:
-	def __init__(self,name,breed,age,color):
-		self.name=name
-		self.breed=breed
-		self.age=age
-		self.color=color
-	def getName(self,):
-		return name
-	def getBreed(self,):
-		return breed
-	def getColor(self,):
-		return color
-	def getAge(self,):
-		return age
-	def setName(self,Name):
-		self.name=Name
-	def setBreed(self,Breed):
-		self.breed=Breed
-	def setColor(self,Color):
-		self.color=Color
-	def setAge(self,Age):
-		self.age=Age
-dog1=Dog("Kovu","German Shepherd",2,"Brown")
-print(dog1.name)
-print(dog1.breed)
-dog1.setName("Kovo")
-print(dog1.name)
+	def __init__(self,num1,num2):
+		self.Number1=self.num1
+		self.Number2=self.num2
+	def getNum(self,):
+		return self.Number1
+	def test(self,number3):
+		number3 = input() 
+		self.Number2 = input() 
+		print(number3)
+		print(self.Number2)
